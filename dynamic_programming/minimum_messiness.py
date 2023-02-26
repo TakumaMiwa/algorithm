475e5aeb5b69346d36d0f4137bf6ba29c8bc18de
TakumaMiwa <takuma.code@gmail.com>
2023-02-26 17:32:41 +0900

    add minimum_messiness.py

---
+++ dynamic_programming/minimum_messiness.py
@@ -0,0 +1,24 @@
+def minimum_messiness(words, line_length):
+    num_remaining_blanks = line_length - len(words[0])
+
+    min_messiness = ([num_remaining_blanks**2] + [float('inf')] * (len(words)-1))
+
+    for i in range(1, len(words)):
+        num_remaining_blanks = line_length - len(words[i])
+        min_messiness[i] = min_messiness[i-1] + num_remaining_blanks**2
+
+        for j in reversed(range(i)):
+            num_remaining_blanks -= len(words[j]) + 1
+            if num_remaining_blanks < 0: break
+            first_j_messiness = 0 if j-1<0 else min_messiness[j-1]
+            current_line_messiness = first_j_messiness + num_remaining_blanks**2
+            min_messiness[i] = min(min_messiness[i], current_line_messiness)
+
+    return min_messiness[-1]
+
+## test
+words = "aaa bbb c d ee ff ggggggg"
+words = words.split()
+line_length = 11
+
+print(minimum_messiness(words, line_length))
